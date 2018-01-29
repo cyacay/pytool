409ed25775c2b7cff96e1a3d802299324e793ac7
oaygnehc <cyano9623@gmail.com>
2018-01-29 20:08:22 +0800

    find_ring

--- subgraph.py
+++ subgraph.py
@@ -51,3 +51,44 @@ def mtx2list(elem_list,adj_mtx):
 		if elem_i<elem_j:
 			adj_list.append((elem_list[elem_i],elem_list[elem_j]))
 	return adj_list
+def find_ring(data):
+    connectmatrix=np.array(data)
+    dim=connectmatrix.shape
+    numnode=dim[1]
+    Node=np.array(range(0,numnode))
+    pair_dict={}
+    connect_dict={}
+    for node in Node:
+        currentline=connectmatrix[node,:]
+        for currentnode in range(0,numnode):
+            if currentline[currentnode] == 1:
+                pair_dict.setdefault(node,[]).append(currentnode)
+
+    connect=int()
+    for node in range(0,numnode):
+        if node in pair_dict.keys():
+            connect=len(pair_dict[node])
+        else:
+            connect=0
+        connect_dict.setdefault(node,connect)
+
+    while 1 in connect_dict.values():
+        for n in range(0,numnode):
+            if connect_dict[n]==1:
+                connect_dict[n]=0
+                connect_node=pair_dict[n][0]
+                connect_dict[connect_node]=connect_dict[connect_node]-1
+
+    ring_node=[]
+    for i in connect_dict.keys():
+        if connect_dict[i]>0:
+            ring_node.append(i)
+    #print ring_node
+    ring_matrix=np.zeros([numnode,numnode])
+    ring_submatrix=connectmatrix[ring_node]
+    ring_submatrix=ring_submatrix[:,ring_node]
+    #print ring_submatrix
+    for i in range(0,len(ring_node)):
+        for j in range(0,len(ring_node)):
+            ring_matrix[ring_node[i],ring_node[j]]=ring_submatrix[i,j]
+    return ring_matrix
